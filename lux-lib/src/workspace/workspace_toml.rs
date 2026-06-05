e797666d0d9a820a662097a34f8a1e7430c0255a
Marc Jakobi <marc@jakobi.dev>
2026-06-05 21:07:58 +0200

    feat: support globs in workplace member paths

--- lux-lib/src/workspace/workspace_toml.rs
+++ lux-lib/src/workspace/workspace_toml.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use serde::Deserialize;
+use serde::{de, Deserialize};
 
 /// The `lux.toml` file for a workspace.
 /// Used to deserialize a workspace with multiple projects.
@@ -9,16 +9,40 @@ pub(super) struct WorkspaceToml {
     pub workspace: WorkspaceSpec,
 }
 
-/// The `lux.toml` file for a workspace.
-#[derive(Clone, Debug, Deserialize)]
-pub(super) struct WorkspaceSpec {
-    /// Paths (relative to the workspace root) of projects to include in the workspace.
-    pub members: Vec<PathBuf>,
-}
-
 impl WorkspaceToml {
     pub(super) fn new(toml_content: &str) -> Result<Self, toml::de::Error> {
         toml::from_str(toml_content)
+    }
+}
+
+/// The `lux.toml` file for a workspace.
+#[derive(Clone, Debug, Deserialize)]
+pub(super) struct WorkspaceSpec {
+    pub members: Vec<WorkspaceMemberSpec>,
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub(super) enum WorkspaceMemberSpec {
+    /// Glob for a path (relative to the workspace root) of projects to include in the workspace.
+    RelativeProjectGlob(String),
+    /// Path (relative to the workspace root) of projects to include in the workspace.
+    RelativeProjectPath(PathBuf),
+}
+
+impl<'de> Deserialize<'de> for WorkspaceMemberSpec {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let path_or_glob = String::deserialize(deserializer)?;
+        Ok(match path_or_glob.strip_prefix("glob:") {
+            Some(pattern) => {
+                // Fail to deserialize if glob can't parse the pattern
+                let _ = glob::Pattern::new(pattern).map_err(de::Error::custom)?;
+                WorkspaceMemberSpec::RelativeProjectGlob(pattern.into())
+            }
+            None => WorkspaceMemberSpec::RelativeProjectPath(PathBuf::from(path_or_glob)),
+        })
     }
 }
 
@@ -33,12 +57,17 @@ mod tests {
             members = [
                 "foo",
                 "projects/bar",
+                "glob:projects/baz/*",
             ]
         "#;
         let workspace_toml = WorkspaceToml::new(toml_content).unwrap();
         assert_eq!(
             workspace_toml.workspace.members,
-            vec![PathBuf::from("foo"), PathBuf::from("projects/bar")]
+            vec![
+                WorkspaceMemberSpec::RelativeProjectPath(PathBuf::from("foo")),
+                WorkspaceMemberSpec::RelativeProjectPath(PathBuf::from("projects/bar")),
+                WorkspaceMemberSpec::RelativeProjectGlob("projects/baz/*".into()),
+            ]
         );
     }
 }
