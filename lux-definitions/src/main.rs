29807f238206ebd6ecbc89e2e08372e49bc26bb8
Marc Jakobi <marc@jakobi.dev>
2026-06-24 15:55:51 +0200

    build(lua-definitions): fail on undocumented params

--- lux-definitions/src/main.rs
+++ lux-definitions/src/main.rs
@@ -19,11 +19,18 @@ fn main() {
         .unwrap_or_else(|e| panic!("Failed to create lua output directory: {e}"));
     let gen = DefinitionFileGenerator::new(definitions.clone());
     for (name, writer) in gen.iter() {
-        let path = lua_dir.join(&name);
+        let lua_output_path = lua_dir.join(&name);
+        let temp_file = lua_dir.join(format!("{}.part", &name));
         writer
-            .write_file(&path)
-            .unwrap_or_else(|e| panic!("Failed to write {}: {e}", path.display()));
-        println!("Generated {}", path.display());
+            .write_file(&temp_file)
+            .unwrap_or_else(|e| panic!("Failed to write {}: {e}", lua_output_path.display()));
+        let content = std::fs::read_to_string(&temp_file).unwrap();
+        if content.contains("@param param") {
+            std::fs::remove_file(&temp_file).unwrap();
+            panic!("Generated definitions with undocumented `@param`");
+        }
+        std::fs::rename(temp_file, &lua_output_path).unwrap();
+        println!("Generated {}", &lua_output_path.display());
     }
 
     // Luau .d.luau files
