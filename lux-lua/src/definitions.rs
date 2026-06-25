e6e9ab2c3a5e2f8a2e76def4c2d2a785120a1695
vhyrro <vhyrro@gmail.com>
2026-06-25 18:35:11 +0200

    fix(definitions): manually define the Lux lua module because of weird behaviour in mlua-extras where the members of Lux are ignored

--- lux-lua/src/definitions.rs
+++ lux-lua/src/definitions.rs
@@ -1,4 +1,5 @@
 use mlua_extras::typed::generator::{Definition, Definitions};
+use mlua_extras::typed::{Type, TypedClassBuilder};
 
 pub struct LuxDefinition {
     pub name: &'static str,
@@ -8,11 +9,40 @@ pub struct LuxDefinition {
 inventory::collect!(LuxDefinition);
 
 pub fn definitions() -> Definitions {
+    // Construct the Lux class manually, as mlua-extras has weird behaviour
+    // where it silently skips certain UserData classes, which only negatively
+    // affects us when generating the Lux class.
+    let lux_class = TypedClassBuilder::default()
+        .method::<(), ()>("loader", "Load the lux loader into the current Lua session")
+        .field(
+            "config",
+            Type::named("ConfigModule"),
+            "Module for building a Lux `Config`",
+        )
+        .field(
+            "workspace",
+            Type::named("WorkspaceModule"),
+            "Module for interacting with a Lux workspace",
+        )
+        .field(
+            "project",
+            Type::named("ProjectModule"),
+            "Module for interacting with a Lux project",
+        )
+        .field(
+            "operations",
+            Type::named("OperationsModule"),
+            "Module for Lux operations",
+        )
+        .build();
+
     let def = inventory::iter::<LuxDefinition>
         .into_iter()
+        .filter(|item| item.name != "LuxModule")
         .fold(Definition::start(), |def, item| {
             def.register_as(item.name, (item.build)())
         })
+        .register_as("LuxModule", Type::class(lux_class))
         .value::<crate::LuxModule>("lux")
         .finish();
 
