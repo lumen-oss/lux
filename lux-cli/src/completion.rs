708ab8cbcd07d0d5ec7a24214275cb8b90736f91
Marc Jakobi <marc@jakobi.dev>
2026-08-02 18:12:29 +0200

    feat(completion): support specifying output directory
    Signed-off-by: Marc Jakobi <marc@jakobi.dev>

--- lux-cli/src/completion.rs
+++ lux-cli/src/completion.rs
@@ -1,33 +1,48 @@
 use std::env;
+use std::io;
 use std::path::PathBuf;
 use std::str::FromStr;
 
 use clap::Args;
 use clap::CommandFactory;
 use clap_complete::generate as clap_generate;
+use clap_complete::generate_to;
 use clap_complete::Shell;
 use miette::miette;
+use miette::IntoDiagnostic;
 use miette::Result;
+use tokio::fs;
 
 use crate::Cli;
 
 #[derive(Args)]
 pub struct Completion {
     /// The shell to generate the completion script for.{n}
-    /// If not set, Lux will try to detect the current shell.{n}
+    /// If not set, and no target directory is specified,{n}
+    /// ux will try to detect the current shell.{n}
+    /// If not set, and a target directory is specified,{n}
+    /// Lux will generate completions for all supported shells.
     /// Possible values: "bash", "elvish", "fish", "powershell", "zsh"{n}
     #[arg(value_enum)]
     shell: Option<Shell>,
+
+    /// The target directory in which to save completions.
+    /// If not set, Lux will print the completions to stdout.
+    #[arg(long)]
+    target_dir: Option<PathBuf>,
 }
 
 pub async fn completion(args: Completion) -> Result<()> {
-    let shell = match args.shell {
-        Some(shell) => shell,
+    let cmd = &mut Cli::command();
+    match &args.target_dir {
         None => {
-            let shell_var: PathBuf = env::var("SHELL")
-                .map_err(|_| {
-                    miette!(
-                        r#"could not auto-detect the shell
+            let shell = match args.shell {
+                Some(shell) => shell,
+                None => {
+                    let shell_var: PathBuf = env::var("SHELL")
+                        .map_err(|_| {
+                            miette!(
+                                r#"could not auto-detect the shell
 Please make sure the SHELL environment variable is set
 or specify the shell for which to generate completions.
 
@@ -35,28 +50,51 @@ Example: `lx completion zsh`
 
 Supported shells: "bash", "elvish", "fish", "powershell", "zsh"
 "#
-                    )
-                })?
-                .into();
-            let shell_name = shell_var
-                .file_name()
-                .unwrap_or_else(|| shell_var.as_os_str())
-                .to_string_lossy();
-            Shell::from_str(&shell_name).map_err(|_| {
-                miette!(
-                    r#"unsupported shell: {}.
+                            )
+                        })?
+                        .into();
+                    let shell_name = shell_var
+                        .file_name()
+                        .unwrap_or_else(|| shell_var.as_os_str())
+                        .to_string_lossy();
+                    Shell::from_str(&shell_name).map_err(|_| {
+                        miette!(
+                            r#"unsupported shell: {}.
 Please specify the shell for which to generate completions.
 
 Example: `lx completion zsh`
 
 Supported shells: "bash", "elvish", "fish", "powershell", "zsh"
 "#,
-                    &shell_name
-                )
-            })?
+                            &shell_name
+                        )
+                    })?
+                }
+            };
+            clap_generate(shell, cmd, "lx", &mut std::io::stdout());
+        }
+        Some(target_dir) => {
+            fs::create_dir_all(&target_dir)
+                .await
+                .map_err(|err| {
+                    io::Error::other(format!(
+                        "error creating directory '{}': {}",
+                        target_dir.display(),
+                        err
+                    ))
+                })
+                .into_diagnostic()?;
+            use clap::ValueEnum;
+            let shells = match args.shell {
+                Some(shell) => &[shell],
+                None => Shell::value_variants(),
+            };
+            for shell in shells {
+                let output = generate_to(*shell, cmd, "lx", target_dir).into_diagnostic()?;
+                tracing::info!("generated {}", output.display());
+            }
         }
     };
-    clap_generate(shell, &mut Cli::command(), "lx", &mut std::io::stdout());
     Ok(())
 }
 
@@ -73,7 +111,12 @@ mod tests {
             Shell::Elvish,
             Shell::PowerShell,
         ] {
-            completion(Completion { shell: Some(shell) }).await.unwrap();
+            completion(Completion {
+                shell: Some(shell),
+                target_dir: None,
+            })
+            .await
+            .unwrap();
         }
     }
 }
