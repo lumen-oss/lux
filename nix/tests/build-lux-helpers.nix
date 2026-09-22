09a7ba9b473834b863880070feeceeb5d65af8be
vhyrro <vhyrro@gmail.com>
2026-09-22 18:11:44 +0200

    fix(nix): migrate fallo fixture lockfile to v2

--- nix/tests/build-lux-helpers.nix
+++ nix/tests/build-lux-helpers.nix
@@ -1,10 +1,37 @@
 {pkgs}: let
-  src = pkgs.fetchFromGitHub {
+  fetched = pkgs.fetchFromGitHub {
     owner = "NTBBloodbath";
     repo = "fallo";
     rev = "c2efe9ad31c97265afb696b62e90312a6b944dc2";
     hash = "sha256-imyKEDvbn9EWJ8Cm54v5sUKZV2fZuBTT3du+1ipKgW4=";
   };
+
+  # `fallo` still ships a v1 lockfile. Upgrade it to the
+  # current format so the helpers can build the project.
+  # FIXME(vhyrro): Remove this once fallo adopts new lockfile format.
+  upgradedLock = let
+    lock = builtins.fromJSON (builtins.readFile "${fetched}/lux.lock");
+  in
+    builtins.toFile "fallo.lux.lock" (builtins.toJSON (
+      lock
+      // {version = "2.0.0";}
+      // builtins.mapAttrs (_: section:
+        section
+        // {
+          entrypoints = builtins.listToAttrs (map (id: {
+            name = section.rocks.${id}.name;
+            value = id;
+          }) (section.entrypoints or []));
+        })
+      (builtins.removeAttrs lock ["version"])
+    ));
+
+  src = pkgs.runCommand "fallo-src" {} ''
+    mkdir -p $out
+    cp -r ${fetched}/. $out/
+    chmod -R u+w $out
+    cp ${upgradedLock} $out/lux.lock
+  '';
 
   lua = pkgs.lua5_1_lux.override {
     packageOverrides = _final: _prev: {lux-lua = pkgs.lux-lua51.debug;};
@@ -52,17 +79,17 @@ in {
 
   importLuxLock = mkTest "build-lux-helpers-import-lock-test" {
     luxDeps = pkgs.importLuxLock {
-      lockFileContents = builtins.readFile "${src}/lux.lock";
+      lockFileContents = builtins.readFile upgradedLock;
     };
   };
 
   luxLock = mkTest "build-lux-helpers-lux-lock-test" {
-    luxLock = "${src}/lux.lock";
+    luxLock = upgradedLock;
   };
 
   luxVendorDir = mkTest "build-lux-helpers-lux-vendor-dir-test" {
     luxVendorDir = pkgs.importLuxLock {
-      lockFileContents = builtins.readFile "${src}/lux.lock";
+      lockFileContents = builtins.readFile upgradedLock;
     };
   };
 
